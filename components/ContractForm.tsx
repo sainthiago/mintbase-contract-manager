5f448789c16b1c79b159bc461f04c82fcc1f7b7e
Rui Santiago <ruisantiagomr@gmail.com>
2022-10-03 09:07:40 +0100

    wip

--- components/ContractForm.tsx
+++ components/ContractForm.tsx
@@ -32,22 +32,37 @@ const ContractForm = () => {
   });
 
   return (
-    <div>
-      Select your contract
-      <div
-        className={`select-wrapper flex items-center justify-between rounded relative border-2 border-light-green py-1.5 px-3`}
-      >
-        <select
-          id="select"
-          className="bg-transparent focus:outline-none w-full cursor-pointer"
-          //   onChange={(event) => onValueChange(event.target.value)}
+    <>
+      <div>
+        Select your contract
+        <div
+          className={`select-wrapper flex items-center justify-between rounded relative border-2 border-light-green py-1.5 px-3`}
         >
-          {stores.map((store) => (
-            <option value={store}>{store}</option>
-          ))}
-        </select>
+          <select
+            id="select"
+            className="bg-transparent focus:outline-none w-full cursor-pointer"
+            //   onChange={(event) => onValueChange(event.target.value)}
+          >
+            {stores.map((store) => (
+              <option value={store}>{store}</option>
+            ))}
+          </select>
+        </div>
       </div>
-    </div>
+      <div>
+        Add new minter
+        <input />
+      </div>
+      <div>
+        Remove minter
+        <input />
+      </div>
+      <div>
+        Transfer ownership
+        <input />
+      </div>
+      <button>+ NEW</button>
+    </>
   );
 };
 
