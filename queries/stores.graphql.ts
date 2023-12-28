524744622b1387a73b1885c733f818b99dd633ce
ruisantiago <ruisantiagomr@gmail.com>
2023-12-28 12:47:15 +0000

    remove minter logic

--- queries/stores.graphql.ts
+++ queries/stores.graphql.ts
@@ -3,12 +3,8 @@ import { gql } from "@apollo/client";
 export const GET_STORES = gql`
   query GetStores($accountId: String!) {
     mb_store_minters(
-      where: {
-        _or: [
-          { minter_id: { _eq: $accountId } }
+      where: 
           { nft_contracts: { owner_id: { _eq: $accountId } } }
-        ]
-      }
       distinct_on: nft_contract_id
     ) {
       nft_contract_id
